2c6d22bba392f05d517f2f5373b7743712358c07
Kruglov <Kruglpv@swecor.com>
2022-07-01 13:54:46 +0300

    Initial commit

---
+++ src/main/java/Generator.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class Generator {
+    public static void generate(int stringLength, int stringQuantity) {
+        Random random = new Random();
+        try {
+            FileWriter fileWriter = new FileWriter("text.txt", true);
+            for (int j = 0; j < stringQuantity; j++) {
+                char[] chars = new char[stringLength];
+
+                for (int i = 0; i < stringLength; i++) {
+                    chars[i] = (char)(97+ random.nextInt(25));
+
+                }
+                String string = new String(chars);
+                string=string+"\n";
+                fileWriter.write(string);
+                fileWriter.flush();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
