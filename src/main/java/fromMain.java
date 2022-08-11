e1e85dfea729fde7ce92b7daa98e3abc4b239a19
Kruglov <Kruglpv@swecor.com>
2022-08-11 16:19:08 +0300

    новые изменения в мастер3

--- src/main/java/fromMain.java
+++ src/main/java/fromMain.java
@@ -2,7 +2,14 @@ public class fromMain {
     public static void main(String[] args) {
         System.out.println( "commit  fromMain");
         System.out.println("uncommited change1");
-        System.out.println("uncommited change2");
+
+
+
+
+
+        System.out.println("commited change12");
+        System.out.println("commited change12");
+
 
     }
 }
