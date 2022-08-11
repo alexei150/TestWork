c6d59196aa6862667bc8011abdd8640f49cecd99
Kruglov <Kruglpv@swecor.com>
2022-08-11 16:02:13 +0300

    новые изменения в мастер3

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -2,5 +2,11 @@ public class NewMain {
     public static void main(String[] args) {
         System.out.println("изменение в main commited");
 
+        System.out.println("изменение в main commited2");
+        System.out.println("изменение в main commited2");
+        System.out.println("изменение в main commited2");
+
+
+
     }
 }
