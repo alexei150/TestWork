77df74ce2165af29f3484f4dfc16bc6b2b35b487
Kruglov <Kruglpv@swecor.com>
2022-08-12 16:31:15 +0300

    новые изменения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("master");
+        System.out.println("master1");
         System.out.println("master2");
+        System.out.println("master3");
 
     }
 }
