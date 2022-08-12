0522d06e7abc59a7c9e7cb41afa24d33977a7ac6
Kruglov <Kruglpv@swecor.com>
2022-08-12 12:53:13 +0300

    новые изменения

--- src/main/java/Other.java
+++ src/main/java/Other.java
@@ -2,5 +2,6 @@ public class Other {
     public static void main(String[] args) {
         System.out.println("Other");
         System.out.println("Other2");
+        System.out.println("Other222");
     }
 }
