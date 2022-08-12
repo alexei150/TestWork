dc4e50c027cbbff589a6bfbc0e4bf02e0aee2c9d
Kruglov <Kruglpv@swecor.com>
2022-08-12 12:15:14 +0300

    новые изменения в мастер3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ветка master");
-        System.out.println("ветка master");
-        System.out.println("ветка master");
-        System.out.println("ветка master");
+        System.out.println("ветка to base 2");
+
 
 
     }
