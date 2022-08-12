2a9f207fce0b0e4c17ed7a2b7b8807f4ba901fed
Kruglov <Kruglpv@swecor.com>
2022-08-12 12:41:50 +0300

    новые изменения

--- src/main/java/Local.java
+++
@@ -1,5 +0,0 @@
-public class Local {
-    public static void main(String[] args) {
-        System.out.println("продвижение в локал");
-    }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ветка to base 2");
-        System.out.println("ветка to base 2");
-        System.out.println("ветка master rebase");
-        System.out.println("ветка master rebase 2");
-
+        System.out.println("main ");
     }
 }
