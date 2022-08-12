c6f0eb8aa14be48591e731cccfb5ef124fa5cb56
Kruglov <Kruglpv@swecor.com>
2022-08-12 12:21:20 +0300

    новые изменения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ветка to base 2");
         System.out.println("ветка to base 2");
-
-
+        System.out.println("ветка master rebase");
 
     }
 }
