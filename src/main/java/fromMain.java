1d57f5110161ab48fb5b10fefb2949b944586165
Kruglov <Kruglpv@swecor.com>
2022-08-12 12:22:41 +0300

    новые изменения

--- src/main/java/fromMain.java
+++ src/main/java/fromMain.java
@@ -1,9 +1,8 @@
 public class fromMain {
     public static void main(String[] args) {
-        System.out.println( "commit  fromMain");
+        System.out.println( "commit  from toBase ");
 
-        System.out.println( "commit master");
-        System.out.println( "commit master");
+
 
 
 
