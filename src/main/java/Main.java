53dc15a2ce5af17e58df604771c29debd100ebca
Kruglov <Kruglpv@swecor.com>
2022-09-22 10:04:43 +0300

    commit4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ public class Main {
         System.out.println("master1");
         System.out.println("master2");
         System.out.println("master3");
-
         System.out.println("master4");
+
+        System.out.println("another");
 
 
 
